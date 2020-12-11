902ac87dfc0dd0ac11f91ce19fb028dc87f11bdf
HaoCooky97 <hao.nguyennhat97@gmail.com>
2020-12-11 23:55:02 +0700

    Init Commit

---
+++ android/app/src/main/kotlin/tk/theflashcard/the_flashcard/MainActivity.kt
@@ -0,0 +1,12 @@
+package tk.theflashcard.the_flashcard
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
